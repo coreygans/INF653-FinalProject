656e6c8970e6d90bfc33b5c70513ec8707f504b3
Corey <coreygans@gmail.com>
2023-04-23 16:27:11 -0500

    Updated the formatting for the statecode to be case insensitive

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -11,12 +11,12 @@ const getAllStates = (req, res) => {
 
 
 const getState = async (req,res) => {
-    const state = data.statesData.find(state => state.code == req.params.state);
+    const state = data.statesData.find(state => state.code == req.params.state.toUpperCase());
     if (!state) {
         return res.status(404).json({ "message": "Invalid state abbreviation parameter" });
     }
     state.funfacts = [];
-    const funfact = await StatesDB.findOne({stateCode: req.params.state}, 'funfacts').lean();
+    const funfact = await StatesDB.findOne({stateCode: req.params.state.toUpperCase()}, 'funfacts').lean();
     state.funfacts = state.funfacts.concat(funfact.funfacts);
     res.status(200).json(state);
 
